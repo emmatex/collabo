82b862742829b20b4a4df988662a9191e1968726
Ifeanyi <emmanuel.eze@netopng.com>
2019-06-26 13:07:00 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
