2b00c6ae017d1f2c8d8857af9dbcad0e6fc78504
Ifeanyi <emmanuel.eze@netopng.com>
2019-07-05 02:46:19 +0100

    add body parser and 404 page

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
+const express = require('express');
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
+
+const indexRoute = require('./routes/index');
+const authRoute = require('./routes/auth');
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // express-handlebars middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -11,9 +19,12 @@ app.set("view engine", "handlebars");
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-   res.render('index');
-})
+app.use(indexRoute);
+app.use('/access', authRoute);
+
+app.use((req, res, next) => {
+    res.status(400).render('404');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
